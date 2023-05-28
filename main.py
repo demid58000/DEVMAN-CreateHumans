9024610fac251b4bacf0f43d295107d0ab1dd936
demid58000 <demid58000@yandex.ru>
2023-05-28 23:51:07 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,123 @@
+import file_operations
+from faker import Faker
+import random
+
+FAKE = Faker("ru_RU")
+SKILLS_LST = [
+    'Стремительный прыжок',
+    'Электрический выстрел',
+    'Ледяной удар',
+    'Стремительный удар',
+    'Кислотный взгляд',
+    'Тайный побег',
+    'Ледяной выстрел',
+    'Огненный заряд']
+LETTERS_MAPPING = {
+    'а': 'а͠',
+    'б': 'б̋',
+    'в': 'в͒͠',
+    'г': 'г͒͠',
+    'д': 'д̋',
+    'е': 'е͠',
+    'ё': 'ё͒͠',
+    'ж': 'ж͒',
+    'з': 'з̋̋͠',
+    'и': 'и',
+    'й': 'й͒͠',
+    'к': 'к̋̋',
+    'л': 'л̋͠',
+    'м': 'м͒͠',
+    'н': 'н͒',
+    'о': 'о̋',
+    'п': 'п̋͠',
+    'р': 'р̋͠',
+    'с': 'с͒',
+    'т': 'т͒',
+    'у': 'у͒͠',
+    'ф': 'ф̋̋͠',
+    'х': 'х͒͠',
+    'ц': 'ц̋',
+    'ч': 'ч̋͠',
+    'ш': 'ш͒͠',
+    'щ': 'щ̋',
+    'ъ': 'ъ̋͠',
+    'ы': 'ы̋͠',
+    'ь': 'ь̋',
+    'э': 'э͒͠͠',
+    'ю': 'ю̋͠',
+    'я': 'я̋',
+    'А': 'А͠',
+    'Б': 'Б̋',
+    'В': 'В͒͠',
+    'Г': 'Г͒͠',
+    'Д': 'Д̋',
+    'Е': 'Е',
+    'Ё': 'Ё͒͠',
+    'Ж': 'Ж͒',
+    'З': 'З̋̋͠',
+    'И': 'И',
+    'Й': 'Й͒͠',
+    'К': 'К̋̋',
+    'Л': 'Л̋͠',
+    'М': 'М͒͠',
+    'Н': 'Н͒',
+    'О': 'О̋',
+    'П': 'П̋͠',
+    'Р': 'Р̋͠',
+    'С': 'С͒',
+    'Т': 'Т͒',
+    'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠',
+    'Х': 'Х͒͠',
+    'Ц': 'Ц̋',
+    'Ч': 'Ч̋͠',
+    'Ш': 'Ш͒͠',
+    'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠',
+    'Ы': 'Ы̋͠',
+    'Ь': 'Ь̋',
+    'Э': 'Э͒͠͠',
+    'Ю': 'Ю̋͠',
+    'Я': 'Я̋',
+    ' ': ' '
+}
+
+
+def main():
+    runic_skills = []
+    for skill in SKILLS_LST:
+        for letter in skill:
+            skill = skill.replace(letter, LETTERS_MAPPING[letter])
+        runic_skills.append(skill)
+    for i in range(10):
+        first_name = FAKE.first_name()
+        last_name = FAKE.last_name_male()
+        job = FAKE.job()
+        town = FAKE.city()
+        strength = random.randint(3, 18)
+        agility = random.randint(3, 18)
+        endurance = random.randint(3, 18)
+        intelligence = random.randint(3, 18)
+        luck = random.randint(3, 18)
+
+        skill_1, skill_2, skill_3 = random.sample(runic_skills, 3)
+
+        context = {
+            "first_name": first_name,
+            "last_name": last_name,
+            "job": job,
+            "town": town,
+            "strength": strength,
+            "agility": agility,
+            "endurance": endurance,
+            "intelligence": intelligence,
+            "luck": luck,
+            "skill_1": skill_1,
+            "skill_2": skill_2,
+            "skill_3": skill_3,
+        }
+        file_operations.render_template("charsheet.svg", "results/result-{0}.svg".format(i), context)
+
+
+if __name__ == '__main__':
+    main()
